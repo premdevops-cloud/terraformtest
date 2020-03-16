37d42c9f9c33394b3e0e0385a3d08f10d99271d0
premdevops-cloud <60018585+premdevops-cloud@users.noreply.github.com>
2020-03-16 13:07:12 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -26,14 +26,7 @@ resource "aws_security_group" "TerraformTestSG" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  
-  ingress {
-    description = "Port 80"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ 
   
   egress {
     from_port   = 0
